4118dfc1b8259b6680cabc2c9947cec549f07f31
Mohammad Kaif Shaikh <mohammadkaif6268@gmail.com>
2025-02-19 14:01:08 +0530

    first commit

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 const flashcards = [
    {
      id: 1,
@@ -28,27 +28,39 @@ const flashcards = [
  ];
 
 const Card = () => {
-  
+   const [inx ,setInx] = useState(0)
+   const curr = flashcards[inx]
+   const handlePrev=()=>{
+      setInx(inx-1)
+
+   }
+   const handleNext = () =>{
+      setInx(inx+1)
+   }
   return (
     <div>
       <div>
-         {flashcards.map((elem)=>{
-            return(
-               <div key={elem.id}>
-                  <h2>{elem.question}</h2>
-                  <p>{elem.answer}</p>
+         
+            
+               <div>
+                  <h2>{curr.question}</h2>
+                  <p>{curr.answer}</p>
                   <div className='flex  gap-2'>
                     <div>
-                    <button>Prev</button>
+                    <button 
+                    disabled = {inx === 0}
+                     onClick={handlePrev}>Prev</button>
                     </div>
                     <div>
-                    <button>Next</button>
+                    <button
+                    disabled = {inx === flashcards.length-1}
+                     onClick={handleNext}>Next</button>
                     </div> 
 
                   </div>
                </div>
-            )
-         })}
+            
+      
       </div>
     </div>
   )
